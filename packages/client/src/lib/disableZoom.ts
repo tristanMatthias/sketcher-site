5cfae59e21b5832eb4adb9a3246b1d5bbefd4b76
Tristan Matthias <hello@tristanmatthias.com>
2019-12-02 20:42:46 -0500

    fix(client): 100vh problem

--- packages/client/src/lib/disableZoom.ts
+++ packages/client/src/lib/disableZoom.ts
@@ -1,14 +1,21 @@
 (() => {
-  // window.addEventListener('touchmove', e => {
-  //   e.preventDefault();
-  //   return false;
-  // });
-  // window.document.body.addEventListener('touchmove', e => {
-  //   e.preventDefault();
-  //   return false;
-  // });
+  // Prevent zooming
   document.querySelector('#app')!.addEventListener('touchmove', e => {
     e.preventDefault();
     return false;
   });
+
+  // Fix for h 100vh
+  // https://css-tricks.com/the-trick-to-viewport-units-on-mobile/
+  const vh = window.innerHeight * 0.01;
+  // Then we set the value in the --vh custom property to the root of the document
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+  // We listen to the resize event
+  window.addEventListener('resize', () => {
+    // We execute the same script as before
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  });
+
 })();
